5ea15853e41d95cd5f564c743a352386da61ce9c
acanedo92 <anahi.canedotx@gmail.com>
2019-08-01 14:16:53 -0500

    Refranes negativos mexicanos, muy divertidos (añadidos)

---
+++ R/RefranesNegativos.R
@@ -0,0 +1,5 @@
+RefranesNegativos <- c("Para todo mal mezcal, para todo bien también",
+"Emborrachandote no logras nada, pero sobrio tampoco",
+"Manda tomo a la chingada mientras te tomas un café", 
+"No existe nada más liberador que mandar todo a la chingada y empezar de nuevo",
+"Chingue su madre, que sea lo que Dios quiera")
