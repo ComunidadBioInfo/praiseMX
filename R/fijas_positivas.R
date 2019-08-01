18eda1ade9cf07553a7ab477d58e978a3d85489e
jemilianosf <jemilianosf@gmail.com>
2019-08-01 14:07:18 -0500

    Añadí las frases que van después de una exclamación positiva.

---
+++ R/fijas_positivas.R
@@ -0,0 +1,6 @@
+fijas_positivas <-  c("Ya la hiciste.",
+                      "Ya estás del otro lado.",
+                      "Te la pelan.",
+                      "Sigue así.",
+                      "Eres unx buenazx.",
+                      "")
