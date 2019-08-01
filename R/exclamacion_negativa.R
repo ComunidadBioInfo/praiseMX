3ec56ba0c3b8ecf8a1bd4049eac68464f3717f41
JazminBlaz <jazminblax@gmail.com>
2019-08-01 15:42:08 -0500

    agregando frases para error

--- R/exclamacion_negativa.R
+++ R/exclamacion_negativa.R
@@ -1,6 +1,6 @@
 #Expresiones negativas 1
 
-exclamacion_negativa1 <- c("Chale,","Nel", "Cámara","",
+exclamacion_negativa1 <- c("Chale,","Nel", "Cámara", "Qué culero",
                           "No mames", "Está gacho", "Ya nos cayó el chahuistle", "Ya nos llevó el payaso",
                           "A la chingada", "Ya bailó Berta")
 
