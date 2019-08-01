9ebbe8df9c5694d86edcb05193daa27930e4d6ee
Alejandra Medina-Rivera <amedina@liigh.unam.mx>
2019-08-01 13:38:41 -0500

    Lista de comida para celebrar

---
+++ R/comida.R
@@ -0,0 +1,13 @@
+comida_rica <- c(
+  "unos tacos",
+  "unas chelas",
+  "unas gorditas",
+  "una carnita asada",
+  "unos troles",
+  "un elote",
+  "un mezcal",
+  "un tequila",
+  "una guacamalla",
+  "una guajolota",
+  "un café de Veracruz"
+)
