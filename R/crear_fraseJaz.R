44128555491bc15f7496818a41d43ea60732fb90
JazminBlaz <jazminblax@gmail.com>
2019-08-01 17:16:21 -0500

    ...

--- R/crear_fraseJaz.R
+++ R/crear_fraseJaz.R
@@ -1,3 +1,8 @@
-crear_frase <- function (x) {
+crear_frase <- function (x){
+  if (x == error) {
+    elementos <- cbind(sample(exclamacion_negativa), sample(frase_negativa))
+    if (elementos[,2]=="mejor vamos por") {}
+  } else if ( x== exito ){ }
   x
 }
+ sample(refran,)
