41209e70ef14866b9895f9bc4c6e7b5355f6dfd5
JazminBlaz <jazminblax@gmail.com>
2019-08-01 14:16:14 -0500

    subiendo pares de las frases de fracaso

--- R/exclamacion_negativa.R
+++ R/exclamacion_negativa.R
@@ -1,13 +1,6 @@
-#frases_negativo
+#Expresiones negativas 1
 
-exclamacion_negativa2 <- c("a todos nos pasa", "pero ya estas cerca de lograrlo", "pero no te agüites"
-"mejor vamos por 'sample(comida)", "mejor echate un coyotito", "ponte trucha", "pero ya ni pedo")
-
-
-
-#Expresiones
-
-exclamacion_negativa1 <- c("Chale,","Nel", "Cámara",""
+exclamacion_negativa1 <- c("Chale,","Nel", "Cámara","",
                           "No mames", "Está gacho", "Ya nos cayó el chahuistle", "Ya nos llevó el payaso",
                           "A la chingada", "Ya bailó Berta")
 
