35f235639917c826145ccdc49fc17514cbacc523
jemilianosf <jemilianosf@gmail.com>
2019-08-02 10:37:14 -0500

    Añadí puntos a las frases.

--- R/refranes_negativos.R
+++ R/refranes_negativos.R
@@ -1,7 +1,7 @@
-refranes_negativos <- c("Para todo mal mezcal, para todo bien también",
-"Emborrachandote no logras nada, pero sobrio tampoco",
-"Manda tomo a la chingada mientras te tomas un café", 
-"No existe nada más liberador que mandar todo a la chingada y empezar de nuevo",
-"Chingue su madre, que sea lo que Dios quiera", 
+refranes_negativos <- c("Para todo mal mezcal, para todo bien también.",
+"Emborrachandote no logras nada, pero sobrio tampoco.",
+"Manda tomo a la chingada mientras te tomas un café.", 
+"No existe nada más liberador que mandar todo a la chingada y empezar de nuevo.",
+"Chingue su madre, que sea lo que Dios quiera.", 
 "Mejor aquí corrió que aquí quedó… ", 
-"Ni pepsi, dijo la coca", "Relaja la raja y disfruta la fruta")
+"Ni pepsi, dijo la coca.", "Relaja la raja y disfruta la fruta.")
