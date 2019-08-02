55680a8a6adc79d2c8a34b1188c6ceac64d07353
Alejandra Medina-Rivera <amedina@liigh.unam.mx>
2019-08-02 11:57:04 -0500

    Modified text

--- tests/testthat/test-set_praisemx.R
+++ tests/testthat/test-set_praisemx.R
@@ -2,5 +2,5 @@ context("test-set_praisemx")
 
 test_that("Se modificaron las opciones de ambiente de R", {
   options_error<-options()$error
-  expect_false(is.null(options_error))
+  expect_match(as.character(options_error),".+refranes.+")
 })
