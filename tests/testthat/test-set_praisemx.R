876b31b4e4204f9547a8d6939e2eb88abd8edb9e
Alejandra Medina-Rivera <amedina@liigh.unam.mx>
2019-08-02 12:01:33 -0500

    added call to function

--- tests/testthat/test-set_praisemx.R
+++ tests/testthat/test-set_praisemx.R
@@ -1,6 +1,7 @@
 context("test-set_praisemx")
 
 test_that("Se modificaron las opciones de ambiente de R", {
+  set_praisemx()
   options_error<-options()$error
   expect_match(as.character(options_error),".+refranes.+")
 })
