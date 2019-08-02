6e6697b5c4c028e84df08c7172857fe01aa500d1
jemilianosf <jemilianosf@gmail.com>
2019-08-02 10:32:28 -0500

    Añadí puntos a las frases.

--- R/exclamacion_negativa.R
+++ R/exclamacion_negativa.R
@@ -2,6 +2,6 @@
 
 exclamacion_negativa <- c("Chale,","Nel", "Cámara", "Qué culero",
                           "No mames", "Está gacho", "Ya nos cayó el chahuistle", "Ya nos llevó el payaso",
-                          "A la chingada", "Ya bailó Berta")
+                          "A la chingada", "Ya bailó Berta", "No puede ser posible")
 
 
