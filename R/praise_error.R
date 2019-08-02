e595a1748d01249990c8dbc3e56bd289f292880a
JazminBlaz <jazminblax@gmail.com>
2019-08-02 12:06:44 -0500

    documentando las funciones praise_bien y praise_error

--- R/praise_error.R
+++ R/praise_error.R
@@ -1,12 +1,17 @@
 #' Crear frases de error estilo mexicanas
 #' 
-#' @description Esta función genera diferentes frases a parir de un muestreo aleatorio de exclamaciones y frases, o refranes, comunes en el vocabulario dominguero mexicano. 
+#' @description Esta función genera frases chidas a parir de un muestreo aleatorio de exclamaciones y frases, o refranes, comunes en el vocabulario dominguero mexicano.
 #' 
-#' @param exclamaciones Colección de exclamaciones que expresan fracaso 
-#' @param frases Colección de frases para motivar un poco 
-#' @param refranes Refranes cotidianos de fracaso
+#' @param exclamaciones Un vector de caracteres formado por una colección de exclamaciones que expresan frustración  
+#' @param frases Un vector de caracteres formado por una colección de frases para motivar un poco 
+#' @param refranes Un vector de caracteres formado por refranes cotidianos de fracaso
+#'
+#' @family crear_frases
+#' 
+#' @return Vector de caracteres
 #'
 #' 
+#'  
 #'    
 #'          
 praise_error <- function(exclamaciones = exclamacion_negativa,
