8ea1e70810de21838a8802437bcc084fc097ab0b
Alejandra Medina-Rivera <amedina@liigh.unam.mx>
2019-08-02 10:28:51 -0500

    removed refranes

--- R/RefranesNegativos.R
+++
@@ -1,5 +0,0 @@
-RefranesNegativos <- c("Para todo mal mezcal, para todo bien también",
-"Emborrachandote no logras nada, pero sobrio tampoco",
-"Manda tomo a la chingada mientras te tomas un café", 
-"No existe nada más liberador que mandar todo a la chingada y empezar de nuevo",
-"Chingue su madre, que sea lo que Dios quiera")
