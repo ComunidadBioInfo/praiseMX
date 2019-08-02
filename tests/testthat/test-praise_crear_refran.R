164db3d38f5a1b20d90327b17f7dc40a1350bba8
jemilianosf <jemilianosf@gmail.com>
2019-08-02 11:01:27 -0500

    Añadir prueba crear refran.

--- tests/testthat/test-praise_crear_refran.R
+++ tests/testthat/test-praise_crear_refran.R
@@ -5,3 +5,8 @@ test_that("praise_crear_refran regresa un character", {
     expect_type(praise_crear_refran(refranes = refranes_positivos),"character")
   
 })
+
+test_that("praise_crear_refran regresa un vector de longitud 1", {
+  expect_length(praise_crear_refran(refranes = refranes_negativos),1)
+
+})
